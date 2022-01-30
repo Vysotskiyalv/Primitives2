7f5a9f610ff83cb47e251effa681491a04416af1
Alexey Vysotskiy <maracrus811@gmail.com>
2022-01-30 10:18:34 +0500

    Modified Main.java

--- variables/src/Main.java
+++ variables/src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
-  public static void main(String[] args) {
-    int accountrefill = 5000;
-    int rublesFor1Bonus = 100;
-    int totalBonusRubles ;
-    int totalaccount ;
-    if (accountrefill > 1000) {
-      totalBonusRubles = accountrefill/rublesFor1Bonus;
-      totalaccount =accountrefill + totalBonusRubles;
+    public static void main(String[] args) {
+        int accountrefill = 1300;
+        int rublesFor1Bonus = 100;
+        int totalBonusRubles;
+        int totalaccount;
+        if (accountrefill > 1000) {
+            totalBonusRubles = accountrefill / rublesFor1Bonus;
+            totalaccount = accountrefill + totalBonusRubles;
             System.out.println(totalaccount);
-    }else {
-      totalBonusRubles = 0;
-      totalaccount = accountrefill;
-      System.out.println(totalaccount);
+        } else {
+            totalBonusRubles = 0;
+            totalaccount = accountrefill;
+            System.out.println(totalaccount);
+        }
     }
-  }
 }
 
